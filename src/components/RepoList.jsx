4d978593cda4e8358be293829be91b64c975025c
Alexander Malinov <alieksandr.malinov@mail.ru>
2021-04-04 19:10:15 +0300

    Add simple repo list

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { getRepoList } from '../actions/index.js';
+import { ListItem } from './ListItem.jsx';
+
+const actionCreators = { getRepoList };
+const mapStateToProps = ({ repoList }) => ({ repoList });
 
 const RepoList = (props) => {
+  const { repoList } = props;
   return (
-    <ul></ul>
+    <ul className='repo-list'>
+      {repoList.map(item => <ListItem key={_.uniqueId()} repoName={item.name}/>)}
+    </ul>
   );
 };
 
-export default RepoList;
+export default connect(mapStateToProps, actionCreators)(RepoList);
